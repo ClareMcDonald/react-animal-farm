cc87a5c2e34d209a3b6dd5db4feb288ab2cd05af
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-01-25 15:56:20 -0800

    animal  images display

--- src/Animal/Animal.js
+++ src/Animal/Animal.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Animal(props) {
   return <p>
-    <img src={`/images/${props.type}.svg`}/>
+    <img src={`/images/${props.animal.type}.svg`}/>
     {props.animal.name} is a {props.animal.type} who says {props.animal.says}
   </p>;
 }
