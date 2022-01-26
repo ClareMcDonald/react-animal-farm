597c2232921179260101fbf3cba82787c5484c1b
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-01-25 16:50:47 -0800

    styled header and footer

--- src/Animal/Animal.js
+++ src/Animal/Animal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Animal.css';
 
 export default function Animal(props) {
-  return <p>
+  return <p className="animal">
     <img className="animal-img" src={`/images/${props.animal.type}.svg`}/>
     <div className="animal-name">{props.animal.name}</div> is a {props.animal.type} who says {props.animal.says}
   </p>;
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,5 +2,5 @@ import React from 'react';
 import './Header.css';
 
 export default function Header(props) {
-  return <header>Alchemy Animal Farm, by {props.name} </header>;
+  return <header className="header">Alchemy Animal Farm, by {props.name} </header>;
 }
