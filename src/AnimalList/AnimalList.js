5e62a77242717c2f2183651a98259843a2df0601
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-01-25 16:27:34 -0800

    aligned content to make animals render nicely

--- src/AnimalList/AnimalList.js
+++ src/AnimalList/AnimalList.js
@@ -3,7 +3,7 @@ import Animal from '../Animal/Animal.js';
 import './AnimalList.css';
 
 export default function AnimalList(props) {
-  return <div className="animail-list">
+  return <div className="animal-list">
     {
       props.animal.map((animal, i) =>
         <Animal key={animal.name + i} animal={animal} />)
