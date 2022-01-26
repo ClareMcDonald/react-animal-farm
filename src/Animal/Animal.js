c95208ad4ad7c7a40200df1bc6fef7e43442bb00
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-01-25 16:03:15 -0800

    added css files

--- src/Animal/Animal.js
+++ src/Animal/Animal.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './Animal.css';
 
 export default function Animal(props) {
   return <p>
-    <img src={`/images/${props.animal.type}.svg`}/>
-    {props.animal.name} is a {props.animal.type} who says {props.animal.says}
+    <img className="animal-img" src={`/images/${props.animal.type}.svg`}/>
+    <div className="animal-name">{props.animal.name}</div> is a {props.animal.type} who says {props.animal.says}
   </p>;
 }
--- src/AnimalList/AnimalList.js
+++ src/AnimalList/AnimalList.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Animal from '../Animal/Animal.js';
+import './AnimalList.css';
+
 export default function AnimalList(props) {
-  return <div>
+  return <div className="animail-list">
     {
       props.animal.map((animal, i) =>
         <Animal key={animal.name + i} animal={animal} />)
